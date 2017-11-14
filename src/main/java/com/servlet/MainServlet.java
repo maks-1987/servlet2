ef24a41049e8ccda0bbfb58bc2db7d2adf4615eb
maks-1987 <max.moderation@gmail.com>
2017-11-14 17:19:48 +0200

    with web.xml

---
+++ src/main/java/com/servlet/MainServlet.java
@@ -0,0 +1,26 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //PrintWriter out = resp.getWriter();
+        //out.print("<h1>H S</h1>");
+        req.setAttribute("name", "Maxi");
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+
+    }
+    /*@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        super.doPost(req, resp);
+
+    }
+*/
+}
---
+++ src/main/webapp/mypage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: max
+  Date: 14.11.2017
+  Time: 16:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSP</title>
+</head>
+<body>
+<h1>Hello ${name}</h1>
+</body>
+</html>
